2b8085e32b31344e7764a5fec56e306e7ef83ce3
only <776268047@qq.com>
2018-08-22 15:51:44 +0800

    react mobx

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Router, Route, Link, IndexRoute, hashHistory} from 'react-router';
+import * as className from 'classnames';
+import * as mobx from 'mobx';
+import * as mobxReact from 'mobx-react';
+import * as reactBootstrap from 'react-bootstrap';
+import * as reactDatepicker from 'react-datepicker';
+import $ from 'jquery';
+import Config from './config';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.css'
+import './less/adminManage.less';
+import GlobalStore from './stores/GlobalStore';
+import App from './containers/App';
+import Bundle from './bundle.js';
+
+import LoginContainer2 from 'bundle-loader?lazy&name=app-[name]!./containers/adminManage/Login';
+
+const Login2  = (props) => (<Bundle load={LoginContainer2} {...props}>{ (Page) => <Page {...props} />}</Bundle>)
+
+
+const requireAuth = (nextState, replace, next) => {
+  //切换路由时初始化环境
+  GlobalStore.hideAlert();
+  // 本地调试环境不进行auth
+  if (process.env.NODE_ENV === 'development' || process.env.PROD_SERVER === "1.1.1.1:8888") {
+    next();
+    return;
+  }
+  //验证权限
+  $.ajax({
+    type: "GET",
+    url: Config.base.islogin,
+    success: data => {
+      if (data.success) {
+        next();
+      } else {
+        window.location = Config.base.index;
+      }
+    }
+  });
+}
+// <Route path="/home" component={Home}/>
+ReactDom.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Login2}/>
+
+    </Route>
+  </Router>,
+  document.getElementById('root')
+);
